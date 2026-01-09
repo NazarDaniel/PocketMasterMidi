f3646a30bc0d6be0fa57d6056e82a7fd5de61fac
Daniel Araujo <daniel@nazar.net.br>
2026-01-08 22:04:31 -0300

    Reboot

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,43 +1,11 @@
 <template>
-  <!-- outputs -->
-  <q-card>Output devices connected = {{ WebMidi.outputs.length }}</q-card>
-  <q-card v-if="selectedOutput === null">No selected device</q-card>
-  <q-card v-if="selectedOutput !== null"> Selected device: {{ selectedOutput.name }} </q-card>
-  <q-card v-if="!WebMidi.outputs"> No available devices </q-card>
-  <q-card v-if="WebMidi.outputs.length > 0">
-    <div>
-      <q-btn
-        v-for="(output, outputKey) in WebMidi.outputs"
-        :key="outputKey"
-        @click="selectOutput(output)"
-      >
-        {{ output.name }}
-      </q-btn>
-    </div>
-  </q-card>
-  <q-card v-if="selectedOutput !== null">
-    <h6>Test commands</h6>
-    <q-btn @click="selectedOutput.channels[1]?.sendControlChange(25, 127)">Program up</q-btn>
-  </q-card>
-
-  <!-- inputs -->
-  <q-card>Input devices connected = {{ WebMidi.inputs.length }}</q-card>
-  <q-card v-if="selectedInput === null">No selected device</q-card>
-  <q-card v-if="selectedInput !== null"> Selected device: {{ selectedInput.name }} </q-card>
-  <q-card v-if="!WebMidi.inputs"> No available devices </q-card>
-  <q-card v-if="WebMidi.inputs.length > 0">
-    <div>
-      <q-btn
-        v-for="(input, inputKey) in WebMidi.inputs"
-        :key="inputKey"
-        @click="selectInput(input)"
-      >
-        {{ input.name }}
-      </q-btn>
-    </div>
-  </q-card>
-  <q-card v-if="selectedInput !== null">
-    <h6>Input monitor</h6>
+  <q-card v-if="!Midi.enabled"> Midi disabled </q-card>
+  <q-card v-if="Midi.enabled">
+    Midi enabled
+    <div>Available outputs:</div>
+    <q-card v-for="(output, key) in Midi.outputs" :key="key">
+      {{ output.name }}
+    </q-card>
   </q-card>
 </template>
 
@@ -46,23 +14,14 @@ import { onMounted, ref } from 'vue';
 import type { Input, Output } from 'webmidi';
 import { WebMidi } from 'webmidi';
 
+const Midi = ref(WebMidi);
+
 onMounted(() => {
-  WebMidi.enable()
-    .then(() => {
-      console.log('WebMidi enabled!');
-    })
-    .catch((err) => {
-      console.error('WebMidi could not be enabled:', err);
-    });
+  Midi.value.enable({ sysex: true }).catch((err) => {
+    console.error('Unable to enable', err);
+  });
 });
 
-const selectedOutput = ref<Output | null>(null);
-const selectOutput = (output: Output | null) => {
-  selectedOutput.value = output;
-};
-
-const selectedInput = ref<Input | null>(null);
-const selectInput = (input: Input | null) => {
-  selectedInput.value = input;
-};
+const midiInput = ref<null | Input>(null);
+const midiOutput = ref<null | Output>(null);
 </script>
